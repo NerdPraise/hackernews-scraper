9d6e6f28851a1b486b86bb3991d54bd23d916e83
Alabi Feranmi <alabiemmanuelferanmi@gmail.com>
2021-07-30 14:40:41 +0100

    add whitenoise

--- hackernews/settings.py
+++ hackernews/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -156,6 +157,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
